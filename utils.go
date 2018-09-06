c79f55d9995062571334ae4418dee049280d4f8f
Alexander Baryshnikov <dev@baryshnikov.net>
2018-09-06 21:28:33 +0800

    add utils to extract headers

--- utils.go
+++ utils.go
@@ -2,6 +2,7 @@ package fluent
 
 import (
 	"context"
+	"github.com/streadway/amqp"
 	"os"
 	"os/signal"
 )
@@ -21,4 +22,108 @@ func SignalContext(parent context.Context) context.Context {
 		}
 	}()
 	return ctx
+}
+
+func BoolHeader(msg *amqp.Delivery, param string, def bool) bool {
+	if msg.Headers == nil {
+		return def
+	}
+	val, ok := msg.Headers[param]
+	if !ok {
+		return def
+	}
+	tVal, ok := val.(bool)
+	if !ok {
+		return def
+	}
+	return tVal
+}
+
+func StringHeader(msg *amqp.Delivery, param string, def string) string {
+	if msg.Headers == nil {
+		return def
+	}
+	val, ok := msg.Headers[param]
+	if !ok {
+		return def
+	}
+	tVal, ok := val.(string)
+	if !ok {
+		return def
+	}
+	return tVal
+}
+
+func IntHeader(msg *amqp.Delivery, param string, def int64) int64 {
+	if msg.Headers == nil {
+		return def
+	}
+	val, ok := msg.Headers[param]
+	if !ok {
+		return def
+	}
+
+	switch t := val.(type) {
+	case int:
+		return int64(t)
+	case int8:
+		return int64(t)
+	case int16:
+		return int64(t)
+	case int32:
+		return int64(t)
+	case int64:
+		return t
+	case uint:
+		return int64(t)
+	case uint8:
+		return int64(t)
+	case uint16:
+		return int64(t)
+	case uint32:
+		return int64(t)
+	case uint64:
+		return int64(t)
+	default:
+		return def
+	}
+}
+
+func FloatHeader(msg *amqp.Delivery, param string, def float64) float64 {
+	if msg.Headers == nil {
+		return def
+	}
+	val, ok := msg.Headers[param]
+	if !ok {
+		return def
+	}
+
+	switch t := val.(type) {
+	case int:
+		return float64(t)
+	case int8:
+		return float64(t)
+	case int16:
+		return float64(t)
+	case int32:
+		return float64(t)
+	case int64:
+		return float64(t)
+	case uint:
+		return float64(t)
+	case uint8:
+		return float64(t)
+	case uint16:
+		return float64(t)
+	case uint32:
+		return float64(t)
+	case uint64:
+		return float64(t)
+	case float32:
+		return float64(t)
+	case float64:
+		return t
+	default:
+		return def
+	}
 }
